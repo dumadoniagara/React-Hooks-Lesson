a8ecb6b2489a5c7a16333cd330b8225b91d9940a
dumadoniagara <dumadsambora@gmail.com>
2020-08-22 11:25:06 +0700

    lesson2: form with useState

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { v1 as uuid } from 'uuid';
+import NewSongForm from './NewSongForm';
 
 const SongList = () => {
    const [songs, setSongs] = useState([
@@ -8,8 +9,8 @@ const SongList = () => {
       { title: 'Dina tanjakan burangrang', id: 3 },
    ])
 
-   const addSong = () => {
-      setSongs([...songs, { title: 'new song', id: uuid() }])
+   const addSong = (title) => {
+      setSongs([...songs, { title, id: uuid() }])
    }
 
    return (
@@ -19,7 +20,8 @@ const SongList = () => {
                return (<li key={song.id}>{song.title}</li>)
             })}
          </ul>
-         <button onClick={addSong}>Add Song</button>
+         <hr />
+         <NewSongForm addSong={addSong} />
       </div>
    )
 }
