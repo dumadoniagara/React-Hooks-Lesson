10c25838b57b77689f0dc4aa53d9379a70565b5f
dumadoniagara <dumadsambora@gmail.com>
2020-08-22 11:56:01 +0700

    useEffect Hooks

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,17 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { v1 as uuid } from 'uuid';
 import NewSongForm from './NewSongForm';
 
 const SongList = () => {
+   /* initial state */
    const [songs, setSongs] = useState([
       { title: 'Aku bukan boneka', id: 1 },
       { title: 'Lagu Indonesia', id: 2 },
       { title: 'Dina tanjakan burangrang', id: 3 },
    ])
+   const [age, setAge] = useState(20);
+
+   /* initial state end */
 
    const addSong = (title) => {
       setSongs([...songs, { title, id: uuid() }])
    }
+
+   const addAge = () => {
+      setAge(age + 1);
+   }
+
+   /* use effect will run for both state change (data of component)*/
+   useEffect(() => {
+      console.log('useEffect ran', songs);
+   }, [songs, age])
+
+   /* use effect will run just for age-state change */
+   useEffect(() => {
+      console.log('useEffect ran', songs);
+   }, [age])
 
    return (
       <div className="song-list">
@@ -22,6 +40,8 @@ const SongList = () => {
          </ul>
          <hr />
          <NewSongForm addSong={addSong} />
+         <br />
+         <button onClick={addAge}>add 1 to Age: {age}</button>
       </div>
    )
 }
